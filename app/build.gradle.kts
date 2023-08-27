9f9df80c20bbc7139df04890985b5428c0f18373
Maxence Leroy <maxence.leroy3@gmail.com>
2023-08-27 20:00:44 +0200

    build: setup ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import de.fayard.refreshVersions.core.versionFor
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
+    id("com.google.devtools.ksp") apply false
 }
