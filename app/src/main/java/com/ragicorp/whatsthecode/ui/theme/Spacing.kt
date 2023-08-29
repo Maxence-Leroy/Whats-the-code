8a820a104eaa309d3ae7b3a713279b584d7c65af
Maxence Leroy <maxence.leroy3@gmail.com>
2023-08-29 18:04:31 +0200

    feat: improve styling

---
+++ app/src/main/java/com/ragicorp/whatsthecode/ui/theme/Spacing.kt
@@ -0,0 +1,8 @@
+package com.ragicorp.whatsthecode.ui.theme
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val screen = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+}
