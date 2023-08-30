7fe4007594be00a5eee0ee143ec4e4f11ca51313
Maxence Leroy <maxence.leroy3@gmail.com>
2023-08-30 15:43:31 +0200

    feat: add icons in the top bar

--- app/src/main/java/com/ragicorp/whatsthecode/addContact/AddContactScreen.kt
+++ app/src/main/java/com/ragicorp/whatsthecode/addContact/AddContactScreen.kt
@@ -7,7 +7,13 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -41,6 +47,24 @@ internal object AddContact {
                     ),
                     title = {
                         Text(text = stringResource(R.string.addContact_titleScreen))
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                Icons.Default.Close,
+                                contentDescription = stringResource(R.string.addContact_leaveButtonDescription)
+                            )
+                        }
+                    },
+                    actions = {
+                        FilledIconButton(
+                            onClick = { /*TODO*/ },
+                        ) {
+                            Icon(
+                                Icons.Default.Done,
+                                contentDescription = stringResource(R.string.addContact_saveButtonDescription)
+                            )
+                        }
                     }
                 )
             },
