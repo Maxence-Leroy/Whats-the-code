f27a0ef9ea030ed9821185eaf9ad3201379ff847
Maxence Leroy <maxence.leroy3@gmail.com>
2023-08-30 14:32:01 +0200

    feat: create textfield component for the app

---
+++ app/src/main/java/com/ragicorp/whatsthecode/ui/WtcTextField.kt
@@ -0,0 +1,74 @@
+package com.ragicorp.whatsthecode.ui
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+
+sealed class WtcImeAction {
+    data object Default : WtcImeAction()
+    data object Next : WtcImeAction()
+    data object Validate : WtcImeAction()
+}
+
+@Composable
+fun WtcTextField(
+    modifier: Modifier = Modifier,
+    value: MutableState<TextFieldValue>,
+    label: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    singleLine: Boolean = true,
+    keyboardAction: WtcImeAction = WtcImeAction.Next
+) {
+    val focusManager = LocalFocusManager.current
+
+    fun getKeyboardOption(): KeyboardOptions {
+        var imeAction = when (keyboardAction) {
+            WtcImeAction.Default -> ImeAction.Default
+            WtcImeAction.Next -> ImeAction.Next
+            WtcImeAction.Validate -> ImeAction.Done
+        }
+
+        if (!singleLine) {
+            imeAction = ImeAction.None
+        }
+
+        return KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction)
+    }
+
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value.value,
+        onValueChange = { value.value = it },
+        label = { Text(text = label) },
+        singleLine = singleLine,
+        minLines = if (singleLine) 1 else 3,
+        keyboardOptions = getKeyboardOption(),
+        keyboardActions = KeyboardActions {
+            if (keyboardAction == WtcImeAction.Validate) {
+                focusManager.clearFocus()
+                return@KeyboardActions
+            }
+            focusManager.moveFocus(FocusDirection.Down)
+        }
+    )
+}
+
+@Preview
+@Composable
+internal fun WtcTextFieldPreview() {
+    val state = remember { mutableStateOf(TextFieldValue("")) }
+    WtcTextField(value = state, label = "Bonjour")
+}
