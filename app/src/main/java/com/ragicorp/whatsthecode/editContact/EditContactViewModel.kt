f0b73f0e9ef1c20026d9ad7a5b1d9c8be4af1266
Maxence Leroy <maxence.leroy3@gmail.com>
2023-09-01 09:47:32 +0200

    feat: allow contact edition

--- app/src/main/java/com/ragicorp/whatsthecode/editContact/EditContactViewModel.kt
+++ app/src/main/java/com/ragicorp/whatsthecode/editContact/EditContactViewModel.kt
@@ -2,15 +2,36 @@ package com.ragicorp.whatsthecode.editContact
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ragicorp.whatsthecode.library.libContact.ContactDomain
 import com.ragicorp.whatsthecode.library.libContact.ContactRepository
 import com.ragicorp.whatsthecode.ui.ContactModificationViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import java.util.UUID
 
-class EditContactViewModel(private val contactId: UUID, val contactRepository: ContactRepository) :
+class EditContactViewModel(contactId: UUID, val contactRepository: ContactRepository) :
     ContactModificationViewModel, ViewModel() {
+    private val contact = contactRepository.getContactById(contactId)
+        .stateIn(viewModelScope, SharingStarted.Eagerly, null)
+
+    init {
+        viewModelScope.launch {
+            contact.collect { contact ->
+                if (contact != null) {
+                    _name.emit(contact.name)
+                    _phoneNumber.emit(contact.phoneNumber)
+                    _address.emit(contact.address)
+                    _apartmentDescription.emit(contact.apartmentDescription)
+                    _freeText.emit(contact.freeText)
+                }
+            }
+        }
+    }
+
     private val _name = MutableStateFlow("")
     override val name = _name.asStateFlow()
     override val setName: (String) -> Unit = { viewModelScope.launch { _name.emit(it) } }
@@ -33,11 +54,41 @@ class EditContactViewModel(private val contactId: UUID, val contactRepository: C
     override val freeText = _freeText.asStateFlow()
     override val setFreeText: (String) -> Unit = { viewModelScope.launch { _freeText.emit(it) } }
 
-    override val isButtonSaveEnabled = MutableStateFlow(false)
+    override val hasSomethingChanged =
+        combine(contact, name, phoneNumber, address, apartmentDescription, freeText) { array ->
+            val mContact = array[0] as ContactDomain?
+            val mName = array[1] as String
+            val mPhoneNumber = array[2] as String
+            val mAddress = array[3] as String
+            val mApartmentDescription = array[4] as String
+            val mFreeText = array[5] as String
 
-    override val hasSomethingChanged = MutableStateFlow(false)
+            ((mContact?.name ?: "") != mName) || ((mContact?.phoneNumber
+                ?: "") != mPhoneNumber) || ((mContact?.address
+                ?: "") != mAddress) || ((mContact?.apartmentDescription
+                ?: "") != mApartmentDescription) || ((mContact?.freeText ?: "") != mFreeText)
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
+
+    override val isButtonSaveEnabled =
+        combine(hasSomethingChanged, name, address) { mHasSomethingChanged, mName, mAddress ->
+            mHasSomethingChanged && (mName.isNotBlank() || mAddress.isNotBlank())
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
 
     override fun save(color: Int?): UUID {
-        return contactId
+        val contactValue = contact.value ?: throw IllegalStateException()
+
+        val newContact = contactValue.copy(
+            name = _name.value.trim(),
+            phoneNumber = _phoneNumber.value.trim(),
+            address = _address.value.trim(),
+            apartmentDescription = _apartmentDescription.value.trim(),
+            freeText = _freeText.value.trim()
+        )
+
+        viewModelScope.launch {
+            contactRepository.editContact(newContact)
+        }
+
+        return contactValue.id
     }
 }
