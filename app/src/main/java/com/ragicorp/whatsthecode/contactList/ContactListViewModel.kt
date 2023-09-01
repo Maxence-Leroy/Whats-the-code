a7673e5dfd5e47034d3f8192be407e1eb6874745
Maxence Leroy <maxence.leroy3@gmail.com>
2023-09-01 09:53:32 +0200

    feat: sort contact by name

--- app/src/main/java/com/ragicorp/whatsthecode/contactList/ContactListViewModel.kt
+++ app/src/main/java/com/ragicorp/whatsthecode/contactList/ContactListViewModel.kt
@@ -4,7 +4,12 @@ import androidx.lifecycle.ViewModel
 import com.ragicorp.whatsthecode.library.libContact.ContactDomain
 import com.ragicorp.whatsthecode.library.libContact.ContactRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class ContactListViewModel(contactRepository: ContactRepository) : ViewModel() {
-    val contacts: Flow<List<ContactDomain>> = contactRepository.getContacts()
+    val contacts: Flow<List<ContactDomain>> = contactRepository
+        .getContacts()
+        .map { contacts ->
+            contacts.sortedBy { it.address }.sortedBy { it.name }
+        }
 }
