5b43e3e7d127b4bd1289a33276a1beb5aec00943
Maxence Leroy <maxence.leroy3@gmail.com>
2023-11-30 10:57:23 +0100

    build: try to fix appcenter

--- app/src/staging/java/InAppUpdateServiceImpl.kt
+++ app/src/staging/java/InAppUpdateServiceImpl.kt
@@ -6,6 +6,13 @@ import com.microsoft.appcenter.distribute.Distribute
 
 class InAppUpdateServiceImpl : InAppUpdateService {
     override fun start(application: Application) {
-        AppCenter.start(application, BuildConfig.APPCENTER_SECRET, Distribute::class.java)
+        if (!AppCenter.isConfigured()) {
+            AppCenter.start(
+                application,
+                BuildConfig.APPCENTER_SECRET,
+                Distribute::class.java
+            )
+            Distribute.checkForUpdate()
+        }
     }
 }
