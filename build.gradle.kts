3e8f97010be7f511f734b32435841e97bec8d35b
Maxence Leroy <maxence.leroy3@gmail.com>
2024-06-02 18:55:07 +0200

    chore: update to kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.paparazzi) apply false
+    alias(libs.plugins.compose) apply false
 }
 
 buildscript {
